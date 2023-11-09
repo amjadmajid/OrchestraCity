5f3e3eec93cf76215c0d46b01eabe1dcc1cb55a4
Amjad <amjad@AM-MBP.local>
2023-11-09 16:37:51 +0100

    initial code for the orchestra city hotel sensors and server

---
+++ hotel_sensors.py
@@ -0,0 +1,57 @@
+import time
+import paho.mqtt.client as mqtt
+import grovepi
+import math
+import json
+
+# Connect the Grove Ultrasonic Rangers to digital ports D5 and D6
+dht         = 4  # The Sensor goes on digital port 4.
+parking_D5  = 5
+charging_D6 = 6
+
+# MQTT settings
+broker_address  = "localhost"
+port            = 1883
+topic_parking   = "parking"
+topic_charging  = "charging"
+topic_raw       = "sensor/raw_distance"
+topic           = "sensor/temperature_humidity"
+
+# Initialize MQTT client
+client = mqtt.Client("hotel_msgs")
+client.connect(broker_address, port)
+
+while True:
+    try:
+        # Get sensor values
+        parking = grovepi.ultrasonicRead(parking_D5)
+        charging = grovepi.ultrasonicRead(charging_D6)
+        [temp, humidity] = grovepi.dht(dht, 0) # 0 = blue sensor, 1 = white sensor
+
+        # Publish raw distances
+        payload_raw = {"parking": parking, "charging": charging}
+        client.publish(topic_raw, json.dumps(payload_raw))
+
+        # Check if distance is below 5 cm and publish to parking and charging topics
+        payload_parking = 1 if parking < 5 else 0
+        payload_charging = 1 if charging < 5 else 0
+
+        client.publish(topic_parking, payload_parking)
+        client.publish(topic_charging, payload_charging)
+
+        print(f"Parking sensor: {parking} cm, charging sensor: {charging} cm")
+
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            print(f"temp = {temp} C\thumidity = {humidity} %")
+
+            # Publish to MQTT
+            payload = {"temperature": temp, "humidity": humidity}
+            client.publish(topic, json.dumps(payload))
+
+        # Delay for a bit before reading the sensors again
+        time.sleep(1)
+
+    except KeyboardInterrupt:
+        break
+    except IOError:
+        print("Error")
---
+++ hotel_server.py
@@ -0,0 +1,60 @@
+import json
+import paho.mqtt.client as mqtt
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+bucket = "hotel_data"
+org = "amj"
+token = "0jwjsNpZoO6RM6UOXueZz8ez95YS0lNG0DHN5nIWDtr3OBEcC8_ozxDa6Aa46gdn0xio0UNDrDXW6-90UIcRRg=="
+client = InfluxDBClient(url="http://localhost:8086", token=token)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# MQTT settings
+MQTT_BROKER = "10.0.0.52"
+MQTT_PORT = 1883
+TOPICS = ["parking", "charging", "sensor/raw_distance", "sensor/temperature_humidity" ]
+
+charging_level = 0
+
+def on_message(client, userdata, msg):
+    global charging_level
+    try:
+        print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+        topic = msg.topic
+        payload = json.loads(msg.payload.decode("utf-8"))
+
+        if topic == "sensor/raw_distance":
+            point = Point("Ultrasonic").tag("sensor", "raw").field("parking", payload["parking"]).field("charging", payload["charging"])
+        elif topic == "parking":
+            point = Point("parking").tag("sensor", "status").field("status", payload)
+        elif topic == "charging":
+            if payload == 0 : 
+                charging_level = 0 
+            elif payload == 1: 
+                charging_level += 1
+                if charging_level >= 100:
+                    charging_level = 100
+
+            point = Point("charging").tag("sensor", "status").field("status", payload)
+            point = Point("charging_level").tag("sensor", "charging_level").field("level", charging_level)
+        elif topic == "sensor/temperature_humidity":
+                temperature = payload.get("temperature", None)
+                humidity = payload.get("humidity", None)
+                point = Point("environment").tag("location", "living_room").field("temperature", temperature).field("humidity", humidity)
+                write_api.write(bucket, org, point)
+            
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode JSON: {e}")
+
+    write_api.write(bucket, org, point)
+
+# MQTT setup
+client = mqtt.Client()
+client.on_message = on_message
+
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+
+for topic in TOPICS:
+    client.subscribe(topic)
+
+client.loop_forever()
